e56228a0bdada54b884d07b239597c418662e6ef
eliasasli <EA210847@reigate.ac.uk>
2022-09-14 21:15:49 +0100

    yes

---
+++ src/ResutSet.java
@@ -0,0 +1,2 @@
+public class ResutSet {
+}
---
+++ src/javaGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class javaGUI {
+    private JPanel Main;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+}
