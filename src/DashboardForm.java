8654f69386982975dbda47686fe0af3ef694dd78
EA210847 <EA210847@reigate.ac.uk>
2022-11-17 10:59:27 +0000

    yyyT

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -11,7 +11,31 @@ public class DashboardForm extends JFrame {
     private JPanel dashboardPanel;
     private JLabel lbAdmin;
     private JButton btnRegister;
+    private JButton btnLogin;
+    private JButton btnBooking;
+    public void table() throws Exception{
+        Connection connection = DriverManager.getConnection("jdbc:ucanaccess://X/My Documents/Database.accdb");
 
+         Statement st = connection.createStatement();
+         ResultSet resultset = st.executeQuery("Select * from tables");
+         while (resultset.next())
+           System.out.println(resultset.getString(1) + "\t" + resultset.getString(2));
+         connection.close();
+
+
+
+
+         MAKE A 3D ARRAY!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+         int tableplacement[][][][][][][][][][] = new int [][][];
+         int flight = 0;
+         while (flight < 5) {
+             if ()
+    }
     public DashboardForm() throws Exception {
         setTitle("Main Menu");
         setContentPane(dashboardPanel);
@@ -48,25 +72,46 @@ public class DashboardForm extends JFrame {
                 }
             }
         });
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginForm loginForm = new LoginForm(DashboardForm.this);
+                User user = loginForm.user;
+
+                if (user != null) {
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "New user: " + user.name,
+                            "you are logged in!",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+        btnBooking.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
-    private boolean connectToDatabase() throws Exception {
-        boolean hasRegistredUsers = false;
-
-        Connection connection = DriverManager.getConnection("jdbc:ucanaccess:/X/My Documents/Database.accdb");
-
-        Statement st = connection.createStatement();
-        ResultSet resultset = st.executeQuery("Select * from user");
-        while (resultset.next())
-            System.out.println(resultset.getString(1) + "\t" + resultset.getString(2));
-
-        connection.close();
-
-        return hasRegistredUsers;
     }
+
+   // private boolean connectToDatabase() throws Exception {
+      //  boolean hasRegistredUsers = false;
+
+       // Connection connection = DriverManager.getConnection("jdbc:ucanaccess:/X/My Documents/Database.accdb");
+
+       // Statement st = connection.createStatement();
+       // ResultSet resultset = st.executeQuery("Select * from user");
+       // while (resultset.next())
+         //   System.out.println(resultset.getString(1) + "\t" + resultset.getString(2));
+
+       // connection.close();
+
+       // return hasRegistredUsers;
+   // }
 
     public static void main(String[] args) throws Exception {
         DashboardForm myForm = new DashboardForm();
     }
 }
-
